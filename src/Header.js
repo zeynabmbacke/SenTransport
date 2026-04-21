f73d95d2cc29b23319cc18ef0dee12d93340a45e
Saynabou Mbacke <nadoushmbacke91@gmail.com>
2026-04-21 16:11:42 +0000

    Setup initial SenTransport avec Header

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header() {
+
+    return (
+
+        <header className ="header">
+
+            <h1 className ="header-titre">SenTransport</h1>
+            
+            <p className ="header-soustitre">
+
+                Votre guide du transport en commun à Dakar
+
+            </p>
+
+        </header>
+    );
+
+}
+
+export default Header;
